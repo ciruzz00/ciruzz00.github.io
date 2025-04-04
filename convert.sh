3cb364d7466a949393a67a8856e1c0c9fe447991
ciruzz00 <190584688+ciruzz00@users.noreply.github.com>
2025-04-04 13:32:24 +0000

    mod-file-conver-and-workflows

--- convert.sh
+++ convert.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-# Convert .MD to .HTML
+# Converte tutti i file .md in .html
 for file in posts/*.md; do
     output_file="public/posts/$(basename "$file" .md).html"
     pandoc "$file" -o "$output_file" --standalone --no-highlight --to=html5 --css="../css/style-post.css"
-   
+
+    # Aggiunge favicon subito dopo il link al CSS
+    sed -i '/<link rel="stylesheet" href="\.\.\/css\/style-post\.css" \/>/a \
+<link rel="icon" type="image/png" href="../img/favicon.ico">' "$output_file"
 done
-sed -i '/<link rel="stylesheet" href="\.\.\/css\/style-post\.css" \/>/a \
-<link rel="icon" type="image/png" href="../img/favicon.ico">' $output_file
 
-# Add files to blog.html
-new_posts=$(ls public/posts/*.html)
-
-# Create links for new posts
+# Genera elenco dei nuovi post
 links=""
-for post in $new_posts; do
+for post in public/posts/*.html; do
     post_name=$(basename "$post" .html)
-    links="$links<li><a href='posts/$post_name.html'>$post_name</a></li>"
+    links+="<li><a href='posts/$post_name.html'>$post_name</a></li>\n"
 done
 
-# Insert new posts between <ul>...</ul> in blog.html
-sed -i '/<ul>/,/<\/ul>/c\<ul>\n'"$links"'\n</ul>' public/blog.html
-
+# Inserisce i link tra <ul> e </ul> in blog.html
+# Protezione dai caratteri speciali usando un file temporaneo
+temp_file=$(mktemp)
+awk -v links="$links" '
+  /<ul>/ { print; print_links=1; next }
+  /<\/ul>/ { print_links=0; print links; print; next }
+  !print_links { print }
+' public/blog.html > "$temp_file" && mv "$temp_file" public/blog.html
