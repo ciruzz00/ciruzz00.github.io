7582d99c11521dd45e6f86ca9d21e1ed76f97f6a
ciruzz00 <190584688+ciruzz00@users.noreply.github.com>
2025-04-05 15:06:49 +0000

    update-file-for-blog

--- convert.sh
+++ convert.sh
@@ -10,18 +10,26 @@ for file in posts/*.md; do
 <link rel="icon" type="image/png" href="../img/favicon.ico">' "$output_file"
 done
 
-# Genera elenco dei nuovi post
+# Genera elenco dei nuovi post con titoli
 links=""
-for post in public/posts/*.html; do
-    post_name=$(basename "$post" .html)
-    links+="<li><a href='posts/$post_name.html'>$post_name</a></li>\n"
+for file in posts/*.md; do
+    post_basename=$(basename "$file" .md)
+    
+    # Estrae il titolo dal file markdown (prima riga che inizia con #)
+    title=$(grep -m 1 "^# " "$file" | sed 's/^# //')
+    
+    # Se non trova un titolo, usa il nome del file
+    if [ -z "$title" ]; then
+        title="$post_basename"
+    fi
+    
+    links+="<li><a href='posts/$post_basename.html'>$title</a></li>\n"
 done
 
 # Inserisce i link tra <ul> e </ul> in blog.html
-# Protezione dai caratteri speciali usando un file temporaneo
 temp_file=$(mktemp)
 awk -v links="$links" '
-  /<ul>/ { print; print_links=1; next }
-  /<\/ul>/ { print_links=0; print links; print; next }
-  !print_links { print }
+  /<ul>/ { print; print links; next }
+  /<\/ul>/ { print; next }
+  { print }
 ' public/blog.html > "$temp_file" && mv "$temp_file" public/blog.html
